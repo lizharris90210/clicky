345319880a7028e8dbbf17318ae7f070f11a177f
Liz Harris <lizharris90210@gmail.com>
2020-01-25 15:53:43 -0500

    getting everything working but images

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+// set the footer to a copyright string
+const Footer = props => (
+  <div className="footer">
+    <p className="text-muted text-center">
+Copyright 2020 Liz Harris     </p>
+  </div>
+);
+
+export default Footer;
