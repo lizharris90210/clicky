a13e6a99d2035c54adc8985e51205b3a9df84397
Liz Harris <lizharris90210@gmail.com>
2020-01-27 15:00:14 -0500

    serviceworker in index path wrong

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
-
